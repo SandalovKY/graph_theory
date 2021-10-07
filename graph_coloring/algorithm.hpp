901f8bc9717891ddec5ad6ae04c0eda198f90b62
SandalovKY <sandaloff.constantin@yandex.ru>
2021-10-07 14:34:23 +0300

    Mod1

--- graph_coloring/algorithm.hpp
+++ graph_coloring/algorithm.hpp
@@ -1,2 +1,8 @@
 #pragma once
 #include <boost/graph/adjacency_list.hpp>
+
+template<typename >
+uint16_t coloringAlgorithm()
+{
+
+}
--- graph_coloring/main.cpp
+++ graph_coloring/main.cpp
@@ -1,6 +1,8 @@
 #include "algorithm.hpp"
 #include "parse_input_json.hpp"
 #include <iostream>
+#include <map>
+#include <utility>
 #include <tuple>
 #include "graph.hpp"
 
@@ -27,19 +29,37 @@ int main(int argc, char ** argv) {
         return EXIT_FAILURE;
     }
 
+    using name_map_t = boost::property_map<my::ColoredGraph, boost::vertex_name_t>::type;
+    using color_map_t = boost::property_map<my::ColoredGraph, boost::vertex_color_t>::type;
+    using colored_graph_descr_t = boost::graph_traits<my::ColoredGraph>::vertex_descriptor;
+
+    using node_pair = std::pair<boost::graph_traits<my::ColoredGraph>::vertex_descriptor, size_t>;
+
+    using my_map_t = std::map<boost::graph_traits<my::ColoredGraph>::vertex_descriptor, size_t>;
+
+    name_map_t mapForNames{};
+    color_map_t mapForColors{};
+
     boost::graph_traits<my::ColoredGraph>::vertex_iterator vi, vi_end;
     boost::graph_traits<my::ColoredGraph>::in_edge_iterator iei, iei_end;
-    boost::graph_traits<my::ColoredGraph>::vertex_descriptor descr;
+    colored_graph_descr_t descr;
     std::tie(vi, vi_end) = boost::vertices(resGraph);
+
+    size_t color{ 0 };
+    my_map_t mapForNodes{};
+
     for (;vi != vi_end; ++vi)
     {
         descr = *vi;
-        std::tie(iei, iei_end) = boost::in_edges(descr, resGraph);
-        for(;iei != iei_end; ++iei)
-        {
-            std::cout << *iei << '\t';
-        }
-        std::cout << '\n';
+        mapForNodes.emplace(std::make_pair(descr, 0));
+        boost::put(mapForNames, descr, "FirstName");
+        boost::put(mapForColors, descr, color++);
+    }
+    std::tie(vi, vi_end) = boost::vertices(resGraph);
+    for (;vi != vi_end; ++vi)
+    {
+        std::cout << boost::get(mapForNames, *vi) << '\n';
+        std::cout << boost::get(mapForColors, *vi) << '\n';
     }
     
     return EXIT_SUCCESS;
